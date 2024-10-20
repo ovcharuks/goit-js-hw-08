0ecccef2fcd2499778425788f9c8b519960bd97c
Сергій Овчарук <ovcharuksvg@gmail.com>
2024-10-20 22:29:27 +0300

    1

--- js/task-3.js
+++
@@ -1,9 +0,0 @@
-const textInput = document.querySelector("#name-input");
-const nameOutput = document.querySelector("#name-output");
-
-textInput.addEventListener("input", (event) => {
-    if(textInput.value.trim() === "" || textInput.value.trim().length === 0){
-        return nameOutput.textContent = "Anonymous";
-    }
-    nameOutput.textContent = event.target.value.trim();}
-);
